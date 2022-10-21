d7e54d43f982bea4de8b2b09d9ff14e7c763211d
bohub12 <qhrb1997@naver.com>
2022-10-21 17:42:11 +0900

    [feature] pSp encoding 구현

--- README.md
+++ README.md
@@ -7,14 +7,51 @@
 #### 라이브러리
 - ninja 설치
 ``` shell
-!wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
-!sudo unzip ninja-linux.zip -d /usr/local/bin/
-!sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force 
+wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+sudo unzip ninja-linux.zip -d /usr/local/bin/
+sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force 
 ```
 
-- pretrained pSp model 다운로드
+- dlib 다운로드
 ``` shell
+pip install cmake
+pip install dlib
+```
 
+
+- pretrained pSp model 다운로드
+``` python
+CODE_DIR = 'pixel2style2pixel'
+
+def get_download_model_command(file_id, file_name):
+    """ Get wget download command for downloading the desired model and save to directory ../pretrained_models. """
+    current_directory = os.getcwd()
+    save_path = os.path.join(os.path.dirname(current_directory), CODE_DIR, "pretrained_models")
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    url = r"""wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={FILE_ID}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id={FILE_ID}" -O {SAVE_PATH}/{FILE_NAME} && rm -rf /tmp/cookies.txt""".format(FILE_ID=file_id, FILE_NAME=file_name, SAVE_PATH=save_path)
+    return url
+
+MODEL_PATHS = {
+    "ffhq_encode": {"id": "1bMTNWkh5LArlaWSc_wa8VKyq2V42T2z0", "name": "psp_ffhq_encode.pt"},
+    "ffhq_frontalize": {"id": "1_S4THAzXb-97DbpXmanjHtXRyKxqjARv", "name": "psp_ffhq_frontalization.pt"},
+    "celebs_sketch_to_face": {"id": "1lB7wk7MwtdxL-LL4Z_T76DuCfk00aSXA", "name": "psp_celebs_sketch_to_face.pt"},
+    "celebs_seg_to_face": {"id": "1VpEKc6E6yG3xhYuZ0cq8D2_1CbT0Dstz", "name": "psp_celebs_seg_to_face.pt"},
+    "celebs_super_resolution": {"id": "1ZpmSXBpJ9pFEov6-jjQstAlfYbkebECu", "name": "psp_celebs_super_resolution.pt"},
+    "toonify": {"id": "1YKoiVuFaqdvzDP5CZaqa3k5phL-VDmyz", "name": "psp_ffhq_toonify.pt"}
+}
+
+path = MODEL_PATHS[experiment_type]
+download_command = get_download_model_command(file_id=path["id"], file_name=path["name"])
+
+# 마지막으로는 download_command 터미널창에서 실행시키기
+# !{download_command}
+```
+
+- shape_predictor_68_face_landmarks.dat 라이브러리 다운로드
+``` shell
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -dk shape_predictor_68_face_landmarks.dat.bz2
 ```
 
 #### 코드 수정
--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ import json
 import os
 from PIL import Image, ExifTags
 from secret import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_REQUEST_SQS_NAME, AWS_RESPONSE_SQS_NAME, AWS_SQS_REGION
-from secret import AWS_RESPONSE_SQS_URL, AWS_REQUEST_SQS_URL, AWS_S3_BUCKET_REGION, AWS_S3_BUCKET_NAME, USER_INPUT_IMAGE_PATH, PREPROCESSING_USER_INPUT_IMAGE_PATH
+from secret import AWS_RESPONSE_SQS_URL, AWS_REQUEST_SQS_URL, AWS_S3_BUCKET_REGION, AWS_S3_BUCKET_NAME, RAW_USER_INPUT_IMAGE_PATH, PREPROCESSING_USER_INPUT_IMAGE_PATH
 import logging
 from botocore.exceptions import ClientError
 
@@ -43,7 +43,8 @@ s3_resource = session.resource('s3')
 
 def download_image_from_s3(user_id):
     # S3로부터 이미지 다운로드 받는다.
-    s3_resource.meta.client.download_file(Bucket=AWS_S3_BUCKET_NAME, Key='/' + str(user_id) + '/' + 'profile.jpg', Filename=USER_INPUT_IMAGE_PATH)
+    s3_resource.meta.client.download_file(Bucket=AWS_S3_BUCKET_NAME, Key='/' + str(user_id) + '/' + 'profile.jpg', Filename=RAW_USER_INPUT_IMAGE_PATH)
+
 
 
 
@@ -89,10 +90,10 @@ def main():
 
             # =================== [Step 2] Barbershop++ 추론 ===================
             # ===== [Step 2.1] face_alignment 실행 - Barbershop.align_face.py (만약, 얼굴이 인식되지 않으면, 얼굴 인식되지 않았다는 에러 메시지 SQS로 송신한다) =====
+
+            image_path = RAW_USER_INPUT_IMAGE_PATH # 알맞게 이미지 path 수정할 것
+
             ## 먼저, face_alignment 시켜줄 수 있도록, 제대로된 방향으로 rotate 시켜준다.
-
-            image_path = USER_INPUT_IMAGE_PATH # 알맞게 이미지 path 수정할 것
-
             try:
                 image = Image.open(image_path)
                 for orientation in ExifTags.TAGS.keys():
@@ -110,15 +111,19 @@ def main():
                 image.save(PREPROCESSING_USER_INPUT_IMAGE_PATH)
 
             except (AttributeError, KeyError, IndexError):
-                logger.exception("Send message failed! (Message body : { result : %s, user_id : %s , image_id : %s})", "이미지 회전 실패", param_user_id, image_id)
+                logger.exception("image preprocessing(rotate) fail!! (Message body : { result : %s, user_id : %s , image_id : %s})", "이미지 회전 실패", param_user_id, image_id)
                 pass
             
+            # 전처리(rotate) 되지 않은 이미지를 삭제한다.
+            delete_raw_input_image_cmd = 'rm  ' + RAW_USER_INPUT_IMAGE_PATH
+            subprocess.call(delete_raw_input_image_cmd, shell=True)
+
             # align face 진행
             os.chdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop")
             subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/align_face.py", shell=True)
             
-            # align 안되어있으면, 실패했다고 리턴함. (폴더에 한 개가 아닌 0개 혹은 2개 생성되면 실패)
-            if len(os.listdir("/content/drive/MyDrive/Barbershop-Plus-Plus/Barbershop/input/face")) != 1:
+            # align 안되어있으면, 실패했다고 리턴함. (폴더에 한 개가 아닌 0개 혹은 2개 생성되면 실패) - reference image는 5개
+            if len(os.listdir("/content/drive/MyDrive/Barbershop-Plus-Plus/Barbershop/input/face")) != 6:
                 fail_body_json = {
                     'result' : 'fail',
                     'user_id' : param_user_id,
@@ -129,20 +134,22 @@ def main():
                 try:
                     # Send message to Request Queue
                     response_queue.send_message(MessageBody=fail_message_body_str, QueueUrl=AWS_RESPONSE_SQS_URL)
-                    logger.info("Send message success! (Message body : { result : %s, user_id : %s , image_id : %s})", "success", param_user_id, image_id)
+                    logger.info("Send fail message success! (Message body : { result : %s, user_id : %s , image_id : %s})", "fail", param_user_id, image_id)
                     continue
                 except ClientError as error:
-                    logger.exception("Send message failed! (Message body : { result : %s, user_id : %s , image_id : %s})", "fail", param_user_id, image_id)
-
-                    raise error  
+                    logger.exception("Send fail message failed! (Message body : { result : %s, user_id : %s , image_id : %s})", "fail", param_user_id, image_id)
+                    continue
+            
 
 
             # ===== [Step 2.2] pSp 인코딩 =====
             os.chdir("/home/ubuntu/Barbershop-Plus-Plus/pixel2style2pixel/")
+            subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/psp_encoding.py", shell=True)
 
 
-            # [Step 2.3] Barbershop 추론
 
+            # ===== [Step 2.3] Barbershop 추론 =====
+            os.chdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/")
 
 
 
