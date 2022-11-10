24bbc9c8795e57c8a77edc5d37a8d8535c28cb96
bohub12 <qhrb1997@naver.com>
2022-11-11 00:10:26 +0900

    [hotfix] 각종 트러블 슈팅

--- inference.py
+++ inference.py
@@ -10,7 +10,6 @@ import logging
 import requests
 from botocore.exceptions import ClientError
 
-
 def get_request_queue():
     aws_session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_SQS_REGION)
     sqs = aws_session.resource('sqs')
@@ -42,9 +41,15 @@ session = boto3.Session(
 )
 s3_resource = session.resource('s3')
 
+### ===========
+reference_cnt = 5
+### ===========
+
+
+
 def download_image_from_s3(member_id, virtual_member_image_id):
     # S3로부터 이미지 다운로드 받는다.
-    s3_resource.meta.client.download_file(Bucket=AWS_S3_BUCKET_NAME, Key='/ai_input/' + str(member_id) + '/' + virtual_member_image_id + '.jpg', Filename=RAW_USER_INPUT_IMAGE_PATH)
+    s3_resource.meta.client.download_file(Bucket=AWS_S3_BUCKET_NAME, Key='ai_input/' + str(member_id) + '/' + str(virtual_member_image_id) + '.jpg', Filename=PREPROCESSING_USER_INPUT_IMAGE_PATH)
 
 def create_presigned_post(object_name,
                           fields=None, conditions=None, expiration=3600):
@@ -114,8 +119,8 @@ def main():
                 ReceiptHandle=message['ReceiptHandle']
             )
 
-            param_member_id = data['member_id']
-            param_virtual_member_image_id = data['virtual_member_image_id']
+            param_member_id = data['memberId']
+            param_virtual_member_image_id = data['virtualMemberImageId']
 
             # AWS S3로부터 유저 이미지 다운로드
             download_image_from_s3(param_member_id, param_virtual_member_image_id)
@@ -123,78 +128,95 @@ def main():
             # =================== [Step 2] Barbershop++ 추론 ===================
             # ===== [Step 2.1] face_alignment 실행 - Barbershop.align_face.py (만약, 얼굴이 인식되지 않으면, 얼굴 인식되지 않았다는 에러 메시지 SQS로 송신한다) =====
 
-            image_path = RAW_USER_INPUT_IMAGE_PATH # 알맞게 이미지 path 수정할 것
-
-            ## 먼저, face_alignment 시켜줄 수 있도록, 제대로된 방향으로 rotate 시켜준다.
-            try:
-                image = Image.open(image_path)
-                for orientation in ExifTags.TAGS.keys():
-                    if ExifTags.TAGS[orientation]=='Orientation':
-                        break
-                exif=dict(image._getexif().items())
-
-                if exif[orientation] == 3:
-                    image=image.rotate(180, expand=True)
-                elif exif[orientation] == 6:
-                    image=image.rotate(270, expand=True)
-                elif exif[orientation] == 8:
-                    image=image.rotate(90, expand=True)
-                    
-                image.save(PREPROCESSING_USER_INPUT_IMAGE_PATH)
-
-            except (AttributeError, KeyError, IndexError):
-                logger.exception("image preprocessing(rotate) fail!! (Message body : { result : %s, member_id : %s , virtual_member_image_id : %s})", "이미지 회전 실패", param_member_id, param_virtual_member_image_id)
-                pass
+            image_path = PREPROCESSING_USER_INPUT_IMAGE_PATH # 알맞게 이미지 path 수정할 것
             
-            # 전처리(rotate) 되지 않은 이미지를 삭제한다.
-            delete_raw_input_image_cmd = 'rm  ' + RAW_USER_INPUT_IMAGE_PATH
-            subprocess.call(delete_raw_input_image_cmd, shell=True)
-
             # align face 진행
             os.chdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop")
             subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/align_face.py", shell=True)
+
+            ## 먼저, face_alignment 시켜줄 수 있도록, 제대로된 방향으로 rotate 시켜준다.
+            try:
+                if len(os.listdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/input/face")) != 6:
+                    image = Image.open(image_path)
+                    for orientation in ExifTags.TAGS.keys():
+                        if ExifTags.TAGS[orientation]=='Orientation':
+                            break
+                    exif=dict(image._getexif().items())
+
+                    if exif[orientation] == 3:
+                        image=image.rotate(180, expand=True)
+                    elif exif[orientation] == 6:
+                        image=image.rotate(270, expand=True)
+                    elif exif[orientation] == 8:
+                        image=image.rotate(90, expand=True)
+                        
+                    image.save(PREPROCESSING_USER_INPUT_IMAGE_PATH)
+
+                    # align face 진행
+                    subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/align_face.py", shell=True)
+
+            except (AttributeError, KeyError, IndexError):
+                logger.exception("image preprocessing(rotate) fail!! (Message body : { result : %s, memberId : %s , virtualMemberImageId : %s})", "이미지 회전 실패", param_member_id, param_virtual_member_image_id)
+                pass
+            
+            # # 전처리(rotate) 되지 않은 이미지를 삭제한다.
+            # delete_raw_input_image_cmd = 'rm  ' + RAW_USER_INPUT_IMAGE_PATH
+            # subprocess.call(delete_raw_input_image_cmd, shell=True)
+
+            # # align face 진행
+            # os.chdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop")
+            # subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/align_face.py", shell=True)
             
             # align 안되어있으면, 실패했다고 리턴함. (폴더에 한 개가 아닌 0개 혹은 2개 생성되면 실패) - reference image는 5개
-            if len(os.listdir("/content/drive/MyDrive/Barbershop-Plus-Plus/Barbershop/input/face")) != 6:
+            if len(os.listdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/input/face")) != 6:
                 fail_body_json = {
                     'result' : 'fail',
-                    'member_id' : param_member_id,
-                    'virtual_member_image_id' : param_virtual_member_image_id
+                    'memberId' : param_member_id,
+                    'virtualMemberImageId' : param_virtual_member_image_id
                 }
 
                 fail_message_body_str = json.dumps(fail_body_json)
+
+                for file in os.listdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/input/face"):
+                    if file == "1.png" or file == "2.png" or file == "3.png" or file == "4.png" or file == "5.png":
+                        continue
+                    delete_cmd = "rm /home/ubuntu/Barbershop-Plus-Plus/Barbershop/input/face/" + file
+                    subprocess.call(delete_cmd, shell=True)
+
                 try:
                     # Send message to Request Queue
                     response_queue.send_message(MessageBody=fail_message_body_str, QueueUrl=AWS_RESPONSE_SQS_URL)
-                    logger.info("Send fail message success! (Message body : { result : %s, member_id : %s , virtual_member_image_id : %s})", "fail", param_member_id, param_virtual_member_image_id)
+                    logger.info("Send fail message success! (Message body : { result : %s, memberId : %s , virtualMemberImageId : %s})", "fail", param_member_id, param_virtual_member_image_id)
                     continue
                 except ClientError as error:
-                    logger.exception("Send fail message failed! (Message body : { result : %s, member_id : %s , virtual_member_image_id : %s})", "fail", param_member_id, param_virtual_member_image_id)
+                    logger.exception("Send fail message failed! (Message body : { result : %s, memberId : %s , virtualMemberImageId : %s})", "fail", param_member_id, param_virtual_member_image_id)
                     continue
             
 
 
             # ===== [Step 2.2] pSp 인코딩 =====
-            # os.chdir("/home/ubuntu/Barbershop-Plus-Plus/pixel2style2pixel/")
-            subprocess.call("/home/ubuntu/Barbershop-Plus-Plus/psp_encoding.py", shell=True)
+            print('psp 호출')
+            os.chdir("/home/ubuntu/Barbershop-Plus-Plus/pixel2style2pixel/")
+            subprocess.call("/home/ubuntu/psp_encoding.py", shell=True)
 
 
 
             # ===== [Step 2.3] Barbershop 추론 =====
+            print('barbershop 호출')
             os.chdir("/home/ubuntu/Barbershop-Plus-Plus/Barbershop/")
-            inference_cmd = 'python3 main.py --sign realistic --smooth 1'
+            inference_cmd = 'python3 main.py --im_path1 input_image.png --sign realistic --smooth 1'
             subprocess.call(inference_cmd, shell=True)
 
             # ===== [Step 3] S3에 inference된 결과 이미지 업로드(presigned url 발급 + s3에 다운로드) =====
             # Generate a presigned S3 POST URL
-            for i in range(5):
-                object_name = '/ai_result/' + param_member_id + '/' + param_virtual_member_image_id + '_' + (i+1) + '.jpg' 
+            for i in range(reference_cnt):
+                object_name = 'ai_result/' + str(param_member_id) + '/' + str(param_virtual_member_image_id) + '_' + str(i+1) + '.jpg' 
                 response = create_presigned_post(object_name)
                 if response is None:
                     continue
 
                 # Demonstrate how another Python program can use the presigned URL to upload a file
-                result_image_name = '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/input_image_' + (i+1) + '_' + (i+1) + '_realistic.png'
+                result_image_name = '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/input_image_' + str(i+1) + '_' + str(i+1) + '_realistic.png'
                 with open(result_image_name, 'rb') as f:
                     files = {'file': (object_name, f)}
                     http_response = requests.post(response['url'], data=response['fields'], files=files)
@@ -205,8 +227,8 @@ def main():
             # ===== [Step 4] 메시지 송신 =====
             success_msg_json = {
                 'result' : 'success',
-                'member_id' : param_member_id,
-                'virtual_member_image_id' : param_virtual_member_image_id
+                'memberId' : param_member_id,
+                'virtualMemberImageId' : param_virtual_member_image_id
             }
 
             success_message_body_str = json.dumps(success_msg_json)
@@ -214,9 +236,9 @@ def main():
             try:
                 # Send message to Request Queue
                 response_queue.send_message(MessageBody=success_message_body_str, QueueUrl=AWS_RESPONSE_SQS_URL)
-                logger.info("Send message success! (Message body : { result : %s, member_id : %s, virtual_member_image_id : %s })", 'success', param_member_id, param_virtual_member_image_id)
+                logger.info("Send message success! (Message body : { result : %s, memberId : %s, virtualMemberImageId : %s })", 'success', param_member_id, param_virtual_member_image_id)
             except ClientError as error:
-                logger.exception("Send message failed! (Message body : { result : %s, member_id : %s, virtual_member_image_id : %s })", 'success', param_member_id, param_virtual_member_image_id)
+                logger.exception("Send message failed! (Message body : { result : %s, memberId : %s, virtualMemberImageId : %s })", 'success', param_member_id, param_virtual_member_image_id)
 
                 raise error
             
@@ -231,20 +253,20 @@ def main():
             subprocess.call(delete_align_input_face_cmd, shell=True)
 
             # output/W+
-            delete_output_W_img_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/W+/input_image.png'
+            # delete_output_W_img_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/W+/input_image.png'
             delete_output_W_npy_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/W+/input_image.npy'
-            subprocess.call(delete_output_W_img_cmd, shell=True)
+            # subprocess.call(delete_output_W_img_cmd, shell=True)
             subprocess.call(delete_output_W_npy_cmd, shell=True)
 
             # output/FS
             delete_output_FS_img_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/FS/input_image.png'
-            delete_output_FS_npy_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/FS/input_image.npy'
+            delete_output_FS_npy_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/FS/input_image.npz'
             subprocess.call(delete_output_FS_img_cmd, shell=True)
             subprocess.call(delete_output_FS_npy_cmd, shell=True)
 
             # result - via_mask
-            for i in range(5):
-                delete_result_mask_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/vis_mask_input_image_' + (i + 1) + '_realistic.png'
+            for i in range(reference_cnt):
+                delete_result_mask_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/vis_mask_input_image_' + str(i + 1) + '_realistic.png'
                 subprocess.call(delete_result_mask_cmd, shell=True)
 
             # result - align
@@ -256,8 +278,8 @@ def main():
             subprocess.call(delete_result_blend_cmd, shell=True)
 
             # result - final
-            for i in range(5):
-                delete_result_real_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/input_image_' + (i + 1) + '_' + (i + 1) + '_realistic.png'
+            for i in range(reference_cnt):
+                delete_result_real_cmd = 'rm ' + '/home/ubuntu/Barbershop-Plus-Plus/Barbershop/output/input_image_' + str(i + 1) + '_' + str(i + 1) + '_realistic.png'
                 subprocess.call(delete_result_real_cmd, shell=True)
 
 if __name__ == "__main__":
--- psp_encoding.py
+++ psp_encoding.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 
 # ====== pSp library ======
@@ -32,110 +34,107 @@ def run_on_batch(inputs, net):
         images = images[:, :, 32:224, :]
     return images, latents
 
+os.chdir("/home/ubuntu/Barbershop-Plus-Plus/pixel2style2pixel/")
 
-if __name__ == '__main__':
-    os.chdir("/home/ubuntu/Barbershop-Plus-Plus/pixel2style2pixel/")
+experiment_type = 'ffhq_encode'
+CODE_DIR = 'pixel2style2pixel'
 
-    experiment_type = 'ffhq_encode'
-    CODE_DIR = 'pixel2style2pixel'
+EXPERIMENT_DATA_ARGS = {
+    "ffhq_encode": {
+        "model_path": "pretrained_models/psp_ffhq_encode.pt",
+        "image_path": "../Barbershop/input/face/input_image.png",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    },
+    "ffhq_frontalize": {
+        "model_path": "pretrained_models/psp_ffhq_frontalization.pt",
+        "image_path": "notebooks/images/input_img.jpg",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    },
+    "celebs_sketch_to_face": {
+        "model_path": "pretrained_models/psp_celebs_sketch_to_face.pt",
+        "image_path": "notebooks/images/input_sketch.jpg",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor()])
+    },
+    "celebs_seg_to_face": {
+        "model_path": "pretrained_models/psp_celebs_seg_to_face.pt",
+        "image_path": "notebooks/images/input_mask.png",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            augmentations.ToOneHot(n_classes=19),
+            transforms.ToTensor()])
+    },
+    "celebs_super_resolution": {
+        "model_path": "pretrained_models/psp_celebs_super_resolution.pt",
+        "image_path": "notebooks/images/input_img.jpg",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            augmentations.BilinearResize(factors=[16]),
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    },
+    "toonify": {
+        "model_path": "pretrained_models/psp_ffhq_toonify.pt",
+        "image_path": "notebooks/images/input_img.jpg",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    },
+}
 
-    EXPERIMENT_DATA_ARGS = {
-        "ffhq_encode": {
-            "model_path": "pretrained_models/psp_ffhq_encode.pt",
-            "image_path": "../Barbershop/unprocessed/input_img.jpg",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-        },
-        "ffhq_frontalize": {
-            "model_path": "pretrained_models/psp_ffhq_frontalization.pt",
-            "image_path": "notebooks/images/input_img.jpg",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-        },
-        "celebs_sketch_to_face": {
-            "model_path": "pretrained_models/psp_celebs_sketch_to_face.pt",
-            "image_path": "notebooks/images/input_sketch.jpg",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.ToTensor()])
-        },
-        "celebs_seg_to_face": {
-            "model_path": "pretrained_models/psp_celebs_seg_to_face.pt",
-            "image_path": "notebooks/images/input_mask.png",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                augmentations.ToOneHot(n_classes=19),
-                transforms.ToTensor()])
-        },
-        "celebs_super_resolution": {
-            "model_path": "pretrained_models/psp_celebs_super_resolution.pt",
-            "image_path": "notebooks/images/input_img.jpg",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                augmentations.BilinearResize(factors=[16]),
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-        },
-        "toonify": {
-            "model_path": "pretrained_models/psp_ffhq_toonify.pt",
-            "image_path": "notebooks/images/input_img.jpg",
-            "transform": transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-        },
-    }
+EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[experiment_type]
 
-    EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[experiment_type]
+model_path = EXPERIMENT_ARGS['model_path']
+ckpt = torch.load(model_path, map_location='cpu')
+opts = ckpt['opts']
 
-    model_path = EXPERIMENT_ARGS['model_path']
-    ckpt = torch.load(model_path, map_location='cpu')
-    opts = ckpt['opts']
+# update the training options
+opts['checkpoint_path'] = model_path
+if 'learn_in_w' not in opts:
+    opts['learn_in_w'] = False
+if 'output_size' not in opts:
+    opts['output_size'] = 1024
 
-    # update the training options
-    opts['checkpoint_path'] = model_path
-    if 'learn_in_w' not in opts:
-        opts['learn_in_w'] = False
-    if 'output_size' not in opts:
-        opts['output_size'] = 1024
+opts = Namespace(**opts)
+net = pSp(opts)
+net.eval()
+net.cuda()
+print('Model successfully loaded!')
 
-    opts = Namespace(**opts)
-    net = pSp(opts)
-    net.eval()
-    net.cuda()
-    print('Model successfully loaded!')
+image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]
+original_image = Image.open(image_path)
+original_image = original_image.convert("RGB")
+original_image.resize((256, 256))
 
-    image_path = EXPERIMENT_DATA_ARGS[experiment_type]["image_path"]
-    original_image = Image.open(image_path)
-    original_image = original_image.convert("RGB")
-    original_image.resize((256, 256))
+# if experiment_type not in ["celebs_sketch_to_face", "celebs_seg_to_face"]:
+#     input_image = run_alignment(image_path)
+# else:
+#     input_image = original_image
 
-    if experiment_type not in ["celebs_sketch_to_face", "celebs_seg_to_face"]:
-        input_image = run_alignment(image_path)
-    else:
-        input_image = original_image
-    
-    input_image.resize((256, 256))
+# input_image.resize((256, 256))
 
-    img_transforms = EXPERIMENT_ARGS['transform']
-    transformed_image = img_transforms(input_image)
+img_transforms = EXPERIMENT_ARGS['transform']
+transformed_image = img_transforms(original_image)
 
-    if experiment_type in ["celebs_sketch_to_face", "celebs_seg_to_face"]:
-        latent_mask = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-    else:
-        latent_mask = None
+if experiment_type in ["celebs_sketch_to_face", "celebs_seg_to_face"]:
+    latent_mask = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+else:
+    latent_mask = None
 
-    with torch.no_grad():
-        tic = time.time()
-        images, latents = run_on_batch(transformed_image.unsqueeze(0), net)
-        toc = time.time()
-        print('Inference took {:.4f} seconds.'.format(toc - tic))
+with torch.no_grad():
+    tic = time.time()
+    images, latents = run_on_batch(transformed_image.unsqueeze(0), net)
+    toc = time.time()
+    print('Inference took {:.4f} seconds.'.format(toc - tic))
 
-    np.save("../Barbershop/output/W+/input_img", latents.cpu().numpy())
-    print(latents.cpu().numpy().shape)
-    
+np.save("../Barbershop/output/W+/input_image", latents.cpu().numpy())
+print(latents.cpu().numpy().shape)
